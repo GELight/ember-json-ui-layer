4ba7b0c2ac2075a913bbd782c0ed2649f4f4c1dc
Mario Linz <mlinz@intershop.de>
2017-03-06 11:25:41 +0100

    Example for an action for ui_b_button component.

--- app/components/ui-b-button/component.js
+++ app/components/ui-b-button/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: '',
+
+  actions: {
+
+    click() {
+      this.set('data.props.title', 'clicked...');
+    }
+
+  }
+
 });
