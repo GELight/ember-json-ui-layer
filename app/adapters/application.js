9dda29877a96fd7286cec88b2a2f67fbd1c67cbc
Mario Linz <gelight@gmx.de>
2017-03-16 11:35:52 +0100

    Update...

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,19 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
+
+  host: 'http://localhost:4200',
+
+  suffix: '.json',
+
+  headers: {
+    'content-type': 'application/json',
+    'data-type': 'application/json'
+  },
+
+  pathForType: function(modelName) {
+    var decamelized = Ember.String.decamelize(modelName);
+    return Ember.String.pluralize(decamelized) + this.get('suffix');
+  }  
+  
 });
